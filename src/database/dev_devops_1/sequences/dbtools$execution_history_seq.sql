7e05f1e399392e25ed220aac64bfb39fb4c6e5f5
sn0313 <ongshinee@gmail.com>
2025-11-12 15:47:44 +0800

    Added test_table and updated schema

---
+++ src/database/dev_devops_1/sequences/dbtools$execution_history_seq.sql
@@ -0,0 +1,5 @@
+create sequence dev_devops_1.dbtools$execution_history_seq minvalue 1 maxvalue 9999999999999999999999999999 increment by 1 start with
+1 cache 20 noorder nocycle nokeep noscale global;
+
+
+-- sqlcl_snapshot {"hash":"3b5e673fe027859932f56dcf643c97b7ed8107ca","type":"SEQUENCE","name":"DBTOOLS$EXECUTION_HISTORY_SEQ","schemaName":"DEV_DEVOPS_1","sxml":"\n  <SEQUENCE xmlns=\"http://xmlns.oracle.com/ku\" version=\"1.0\">\n   <SCHEMA>DEV_DEVOPS_1</SCHEMA>\n   <NAME>DBTOOLS$EXECUTION_HISTORY_SEQ</NAME>\n   <START_WITH>1</START_WITH>\n   <INCREMENT>1</INCREMENT>\n   <MINVALUE>1</MINVALUE>\n   <MAXVALUE>9999999999999999999999999999</MAXVALUE>\n   <CACHE>20</CACHE>\n   <SCALE>NOSCALE</SCALE>\n</SEQUENCE>"}
